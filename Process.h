553d1d2ad150b83e14b0033bccd9840502678f5a
Jason Ghiglieri <jasoncghiglieri@gmail.com>
2019-02-12 12:37:53 -0700

    Process.cpp and .h, main.cpp, gitignore and nbproject added.

---
+++ Process.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   Process.h
+ * Author: jason
+ *
+ * Created on February 12, 2019, 12:22 PM
+ */
+
+#ifndef PROCESS_H
+#define PROCESS_H
+
+class Process {
+public:
+    Process();
+    Process(const Process& orig);
+    virtual ~Process();
+private:
+
+};
+
+#endif /* PROCESS_H */
+
